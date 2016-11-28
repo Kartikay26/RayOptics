c0d045a8dcaed0b16c94986f02ffe2640cd96946
Kumar Kartikay <krkartikay@gmail.com>
2016-11-28 17:06:34 +0530

    Added 'eye' display mechanism
    The mechanism added only shows the output got from the eye. It is not
    the mechanism by which the eye 'see's.
    
    Note: Run this program carefully. (preferably after incresing step to
    5/10). May crash browser.

--- j.js
+++ j.js
@@ -1,6 +1,7 @@
 window.onload = main
 
-var can1,can2,c1,c2,w,h,x0,y0,tau=2*Math.PI,unit,zoom=1,wa=1,wb=2,wc=3;
+var can1,can2,c1,c2,w,h,x0,y0,tau=2*Math.PI,unit,zoom=1,f=10,t = 0,step=1
+// step = 1 for perfect resolution
 
 function change (variableName,to,time) {
 	var i = eval(variableName)
@@ -9,125 +10,106 @@ function change (variableName,to,time) {
 		if(Math.abs("+variableName+"-"+to+")<0.1)\
 		{clearInterval(_z_"+variableName+");_z_"+variableName+"=0;\
 		console.log('Done (change "+variableName+").')}},100)"
-	var n = eval(z);
+	var n = eval(z)
 	eval("_z_"+variableName+"="+n)
-	return n;
+	return n
 }
 
 function main() {
 
 	// get Canvases ready
-	can1 = document.getElementById('firstCanvas');
-	can2 = document.getElementById('secondCanvas');
-	c1 = can1.getContext('2d');
-	c2 = can2.getContext('2d');
-	can1.style.position="absolute";
-	can2.style.position="absolute";
-	w_ = window.innerWidth;
-	h_ = window.innerHeight;
+	can1 = document.getElementById('firstCanvas')
+	can2 = document.getElementById('secondCanvas')
+	c1 = can1.getContext('2d')
+	c2 = can2.getContext('2d')
+	can1.style.position="absolute"
+	can2.style.position="absolute"
+	w_ = window.innerWidth
+	h_ = window.innerHeight
 	if (w_/2>h_) {
-		can1.style.left="0px";
-		can2.style.right="0px";
-		can1.width = w_/2;
-		can1.height = h_;
-		can2.width = w_/2;
-		can2.height = h_;
+		can1.style.left="0px"
+		can2.style.right="0px"
+		can1.width = w_/2
+		can1.height = h_
+		can2.width = w_/2
+		can2.height = h_
 		w=w_/2
 		h=h_
 	}
 	else{
-		can1.style.top="0px";
-		can2.style.bottom="0px";
-		can1.width = w_;
-		can1.height = h_/2;
-		can2.width = w_;
-		can2.height = h_/2;
+		can1.style.top="0px"
+		can2.style.bottom="0px"
+		can1.width = w_
+		can1.height = h_/2
+		can2.width = w_
+		can2.height = h_/2
 		w=w_
 		h=h_/2
-	};
-	x0=w/2;
-	y0=h/2;
+	}
+	x0=w/2
+	y0=h/2
 
 	// DRAW
-
-	c1.lineCap = "round";
-	addPoint(-1,-1,-1);
-	addPoint(-1,-1,1);
-	addPoint(-1,1,-1);
-	addPoint(-1,1,1);
-	addPoint(1,-1,-1);
-	addPoint(1,-1,1);
-	addPoint(1,1,-1);
-	addPoint(1,1,1);
-	addPoint(0,0,0);
-	addLine(new Point(-1,-1,-1), new Point(-1,-1,1));
-	addLine(new Point(-1,-1,-1), new Point(-1,1,-1));
-	addLine(new Point(-1,-1,-1), new Point(1,-1,-1));
-	addLine(new Point(-1,-1,1), new Point(-1,1,1));
-	addLine(new Point(-1,-1,1), new Point(1,-1,1));
-	addLine(new Point(-1,1,-1), new Point(-1,1,1));
-	addLine(new Point(-1,1,-1), new Point(1,1,-1));
-	addLine(new Point(1,-1,-1), new Point(1,1,-1));
-	addLine(new Point(1,-1,-1), new Point(1,-1,1));
-	addLine(new Point(1,1,1), new Point(1,1,-1));
-	addLine(new Point(1,1,1), new Point(1,-1,1));
-	addLine(new Point(1,1,1), new Point(-1,1,1));
-	animate();
-	setTimeout(setInterval,10000,animate,100);
+	drawFrame()
+	c1.lineCap = "round"
+	universe.eye = new Eye(1,1,1,1,1,1)
+	addPoint(0,0,0)
+	animate()
+	setInterval(animate,1000/f)
 }
 
-t = 0;
-
 function drawFrame () {
-	c1.fillStyle = "#111";
-	c1.fillRect(0,0,w,h);
-	c2.fillStyle = "#222";
-	c2.fillRect(0,0,w,h);
-	c2.beginPath();
-	c2.arc(x0,y0,Math.min(w/2,h/2)-5,0,tau);
-	c2.lineWidth = 3;
-	c2.strokeStyle = "#fff";
-	c2.stroke();
-	c2.fillStyle="#000";
-	c2.fill();
-	c2.closePath();
+	c1.fillStyle = "#111"
+	c1.fillRect(0,0,w,h)
+	c2.fillStyle = "#000"
+	c2.fillRect(0,0,w,h)
 }
 
 function animate () {
-	drawFrame();
-	unit = Math.min(w,h)/10 * zoom;
-	universe.display.a += wa/100;
-	universe.display.b += wb/100;
-	universe.display.c += wc/100;
-	t += 1;
-	d = whatToDraw();
+	drawFrame()
+	unit = Math.min(w,h)/10 * zoom
+	t += 1
+	d = whatToDraw()
 	for (var i = d.lines.length - 1; i >= 0; i--) {
-		line = d.lines[i];
-		drawLine(line);
-	};
+		line = d.lines[i]
+		drawLine(line)
+	}
 	for (var i = d.points.length - 1; i >= 0; i--) {
-		point = d.points[i];
-		drawPoint(point);
-	};
+		point = d.points[i]
+		drawPoint(point)
+	}
+
+	// Draw something on the "eye" canvas
+	eyeDraw(universe.eye.see)
 }
 
 function drawPoint (point2d) {
-	var x = point2d.x;
-	var y = point2d.y;
-	c1.beginPath();
-	c1.strokeStyle = "#fff";
-	c1.lineWidth = 5;
-	c1.arc(x0+x,y0-y,5,0,tau);
-	c1.stroke();
-	c1.closePath();
+	var x = point2d.x
+	var y = point2d.y
+	c1.beginPath()
+	c1.strokeStyle = "#fff"
+	c1.lineWidth = 5
+	c1.arc(x0+x,y0-y,5,0,tau)
+	c1.stroke()
+	c1.closePath()
 }
 
 function drawLine (line2d) {
-	c1.beginPath();
-	c1.strokeStyle = "#fff";
-	c1.lineWidth = 5;
-	c1.moveTo(x0+line2d.x1,y0-line2d.y1);
-	c1.lineTo(x0+line2d.x2,y0-line2d.y2);
-	c1.stroke();
-	c1.closePath();
+	c1.beginPath()
+	c1.strokeStyle = "#fff"
+	c1.lineWidth = 5
+	c1.moveTo(x0+line2d.x1,y0-line2d.y1)
+	c1.lineTo(x0+line2d.x2,y0-line2d.y2)
+	c1.stroke()
+	c1.closePath()
+}
+
+function eyeDraw (func) {
+	for (var i = -w; i < w; i+=step) {
+		for (var j = -h; j < h; j+=step) {
+			c2.fillStyle = func(i,j)
+			c2.fillRect(x0+i,y0-j,step,step)
+			// note the last 2 arguments ar not coordinates
+		}
+	}
 }
--- math.js
+++ math.js
@@ -1,34 +1,38 @@
-universe = {'lines':[],'points':[]};
+universe = {'lines':[],'points':[], 'eye':{}}
 
-universe.display = {'a':0, 'b':0, 'c':0};
+universe.display = {'a':0, 'b':0, 'c':0}
+
+var hex = ['0','1','2','3','4','5','6','7','8','9','a','b','c','d','e','f','f']
 
 function addPoint (x,y,z) {
-	universe.points.push(new Point(x,y,z));
+	universe.points.push(new Point(x,y,z))
 }
 
 function addLine (p1,p2) {
-	universe.lines.push(new Line(p1,p2));
+	universe.lines.push(new Line(p1,p2))
 }
 
 function Point (x,y,z) {
-	this.x = x;
-	this.y = y;
-	this.z = z;
+	this.x = x
+	this.y = y
+	this.z = z
 }
 
-origin = new Point(0,0,0);
+origin = new Point(0,0,0)
 
 function Vector (p1,p2) {
-	this.x = p2.x-p1.x;
-	this.y = p2.y-p1.y;
-	this.z = p2.z-p1.z;
+	this.x = p2.x-p1.x
+	this.y = p2.y-p1.y
+	this.z = p2.z-p1.z
 
-	this.toRowVector = function () { return new Matrix([[this.x,this.y,this.z]]);}
-	this.toColumnVector = function () { return new Matrix([[this.x],[this.y],[this.z]]);}
+	this.toRowVector = function () {
+		return new Matrix([[this.x,this.y,this.z]]);}
+	this.toColumnVector = function () {
+		return new Matrix([[this.x],[this.y],[this.z]]);}
 }
 
 function CoordinatesToVector(x,y,z){
-	return new Vector(origin,new Point(x,y,z));
+	return new Vector(origin,new Point(x,y,z))
 }
 
 function Matrix(arr){
@@ -37,109 +41,140 @@ function Matrix(arr){
 	// (rows) v (columns)
 	// E.g., [[1,2,3],
 	//        [4,5,6]]
-	this.arr = arr;
-	this.rows = arr.length;
-	this.columns = arr[0].length;
+	this.arr = arr
+	this.rows = arr.length
+	this.columns = arr[0].length
 	
 	this.Multiply = function (other) {
-		var A = this;
-		var B = other;
+		var A = this
+		var B = other
 		if(A.columns==B.rows){
 			var C = []
 			for (var i = A.rows - 1; i >= 0; i--) {
 				C.push([])
-			};
+			}
 			// Resultant matrix has size a.rows x b.columns
 			for (var i = A.rows - 1; i >= 0; i--) {
 				for (var j = B.columns - 1; j >= 0; j--) {
 					var sum = 0
 					for (var k = 0; k < A.columns; k++) {
 						sum += A.arr[i][k] * B.arr[k][j]
-					};
-					C[i][j] = sum;
-				};
-			};
+					}
+					C[i][j] = sum
+				}
+			}
 		}
 		else{
-			throw "Error: Matrices incompatible for multiplication.";
+			throw "Error: Matrices incompatible for multiplication."
 		}
-		return new Matrix(C);
+		return new Matrix(C)
 	}
 
 	this.toVector = function (){
 		if (this.rows == 1) {
 			var v = this.arr[0]
-			return CoordinatesToVector(v[0],v[1],v[2]);
+			return CoordinatesToVector(v[0],v[1],v[2])
 		} else if (this.columns == 1){
 			var v = []
 			for (var i = this.arr.length - 1; i >= 0; i--) {
-				v[i]=this.arr[i];
-			};
-			return CoordinatesToVector(v[0],v[1],v[2]);
+				v[i]=this.arr[i]
+			}
+			return CoordinatesToVector(v[0],v[1],v[2])
 		} else {
-			throw "Error: This matrix is neither a row nor a column vector.";
-		};
+			throw "Error: This matrix is neither a row nor a column vector."
+		}
 	}
 }
 
 function getRotationMatrix(a,b,c){
 	var A = new Matrix([[+Math.cos(a),+Math.sin(a),0],
 						[-Math.sin(a),+Math.cos(a),0],
-						[0,           0,           1]]);
+						[0,           0,           1]])
 	var B = new Matrix([[+Math.cos(b),0,+Math.sin(b)],
 						[0,           1,           0],
-						[-Math.sin(b),0,+Math.cos(b)]]);
+						[-Math.sin(b),0,+Math.cos(b)]])
 	var C = new Matrix([[1,0,           0           ],
 						[0,+Math.cos(c),+Math.sin(c)],
-						[0,-Math.sin(c),+Math.cos(c)]]);
-	return A.Multiply(B.Multiply(C));
+						[0,-Math.sin(c),+Math.cos(c)]])
+	return A.Multiply(B.Multiply(C))
 }
 
 function Line(initial_point,final_point){
-	this.initial_point = initial_point;
-	this.final_point = final_point;
-	this.direction_ratios = new Vector(initial_point,final_point);
+	this.initial_point = initial_point
+	this.final_point = final_point
+	this.direction_ratios = new Vector(initial_point,final_point)
 }
 
 function CanvasPoint (point) {
 
 	// ISOMETRIC VIEW
 
-	var ry = 1/2;
-	var rx = Math.sqrt(3)/2;
+	var ry = 1/2
+	var rx = Math.sqrt(3)/2
 	var v = new Vector(origin,point); // apply rotation matrix before display
-	v = v.toColumnVector();
+	v = v.toColumnVector()
 	M = getRotationMatrix(universe.display.a,
 						  universe.display.b,
-						  universe.display.c);
-	v = M.Multiply(v);
-	v = v.toVector();
-	var x=v.x,y=v.y,z=v.z;
-	this.x = unit *(0+rx*x-rx*y);
-	this.y = unit *(z-ry*x-ry*y);
+						  universe.display.c)
+	v = M.Multiply(v)
+	v = v.toVector()
+	var x=v.x,y=v.y,z=v.z
+	this.x = unit *(0+rx*x-rx*y)
+	this.y = unit *(z-ry*x-ry*y)
 }
 
 function CanvasLine (line) {
 	// Converts a Line (in 3d) to a line suitable for
 	// drawing on a 2d canvas.
-	this.x2 = new CanvasPoint(line.final_point).x;
-	this.x1 = new CanvasPoint(line.initial_point).x;
-	this.y2 = new CanvasPoint(line.final_point).y;
-	this.y1 = new CanvasPoint(line.initial_point).y;
+	this.x2 = new CanvasPoint(line.final_point).x
+	this.x1 = new CanvasPoint(line.initial_point).x
+	this.y2 = new CanvasPoint(line.final_point).y
+	this.y1 = new CanvasPoint(line.initial_point).y
 }
 
 function whatToDraw (t) {
-	var lines = [];
+	var lines = []
 	for (var i = universe.lines.length - 1; i >= 0; i--) {
-		var l = universe.lines[i];
-		var l_ = new CanvasLine(l);
-		lines.push(l_);
-	};
-	var points = [];
+		var l = universe.lines[i]
+		var l_ = new CanvasLine(l)
+		lines.push(l_)
+	}
+	var points = []
 	for (var i = universe.points.length - 1; i >= 0; i--) {
-		var p = universe.points[i];
-		var p_ = new CanvasPoint(p);
-		points.push(p_);
-	};
-	return {'lines':lines,'points':points};
+		var p = universe.points[i]
+		var p_ = new CanvasPoint(p)
+		points.push(p_)
+	}
+	return {'lines':lines,'points':points}
+}
+
+function Eye (x,y,z,a,b,c) {
+	this.x = x; // ┬
+	this.y = y; // | Position of the eye
+	this.z = z; // ┴
+	this.a = a; // ┬
+	this.b = b; // | Direction in which the eye is watching.
+	this.c = c; // ┴
+	addPoint(x,y,z)
+	this.see = function (x,y) {
+		// supposed to 'send' rays in all directions and 'see objects'
+		// Scheme:
+		// (x,y) +-(a,b,c?) ----> L-Projection ----> (x,y,z) (part of sphere)
+		// -----> L-P ----> all directions ----> rays -----> search objects
+		// ---> follow laws of optics -----> end on opaque object/light source->
+		// ------> object.colour ---> average --------> hex ------> return
+		// returns colour
+		z = 100/(Math.sqrt(x*x+y*y)+1)
+
+		// To_RGB (0-->1) |----> (#xyz)
+		var zr = z, zg = z, zb = z
+		return "#"+hex[Math.floor(zr*16)]
+				  +hex[Math.floor(zg*16)]
+				  +hex[Math.floor(zb*16)]
+	}
+}
+
+function LambertProjection (argument) {
+	// body...
+	return false
 }
